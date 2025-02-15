564dc8fd4e704a8155068d5043bd3e1c64d51999
Saibot57 <tobiaslundh75@gmail.com>
2025-02-15 19:32:47 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,11 +37,14 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${redHat.className} ${monument.variable} min-h-screen bg-[#fcd7d7]`}>
         {/* Top Navigation Bar */}
-        <div className="fixed top-0 left-0 right-0 h-24 border-b-2 border-black bg-[#fcd7d7] z-10">
-          {/* Vertical line for square corner */}
-          <div className="absolute top-0 left-24 h-full border-l-2 border-black"></div>
+        <div className="fixed top-0 left-0 right-0 h-24 bg-[#fcd7d7] z-10">
+          {/* Bottom border that extends full width */}
+          <div className="absolute bottom-0 left-0 right-0 border-b-2 border-black"></div>
+          {/* Additional border that creates the square */}
+          <div className="absolute bottom-0 left-0 w-24 border-b-2 border-black"></div>
+          
           <div className="ml-24 h-full flex items-center px-8">
-            <h1 className="text-2xl font-monument mr-12 ml-8">Bibliotek</h1>
+            <h1 className="text-2xl font-monument mr-12">Bibliotek</h1>
             <nav className="flex space-x-8">
               <span className="text-sm font-monument">Kalender</span>
               <span className="text-sm font-monument">Schema</span>
