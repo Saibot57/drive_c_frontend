3df753a6eafa82ea2dfa1a6a3e8443e9e54b5ddf
Saibot57 <tobiaslundh75@gmail.com>
2025-05-15 13:46:59 +0200

    Update context-menu.tsx

--- src/components/ui/context-menu.tsx
+++ src/components/ui/context-menu.tsx
@@ -100,8 +100,9 @@ function ContextMenuContent({
     <ContextMenuPrimitive.Portal>
       <ContextMenuPrimitive.Content
         data-slot="context-menu-content"
+        style={{ zIndex: 9999 }}
         className={cn(
-          "z-[100] min-w-[12rem] overflow-hidden rounded-base border-2 border-border bg-white p-1 font-base text-black shadow-lg animate-in fade-in-80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 origin-(--radix-context-menu-content-transform-origin)",
+          "min-w-[12rem] overflow-hidden rounded-base border-2 border-border bg-white p-1 font-base text-black shadow-lg animate-in fade-in-80...",
           className,
         )}
         {...props}
