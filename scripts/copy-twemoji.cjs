110c70ea63dffa77f81af0a71cf054bda502076c
Saibot57 <tobiaslundh75@gmail.com>
2025-09-15 21:14:55 +0200

    fix(emoji): simplify twemoji copy and types

--- scripts/copy-twemoji.cjs
+++ scripts/copy-twemoji.cjs
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const srcDir = path.join(__dirname, '..', 'node_modules', 'twemoji', 'assets', 'svg');
+const srcDir = path.join(__dirname, '..', 'node_modules', '@twemoji', 'svg');
 const destDir = path.join(__dirname, '..', 'public', 'twemoji');
 
 fs.mkdirSync(destDir, { recursive: true });
