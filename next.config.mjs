e59b2af71a07346a094bd9a2df4e5a8b93642f91
Saibot57 <tobiaslundh75@gmail.com>
2026-04-11 12:24:09 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -21,7 +21,7 @@ const nextConfig = {
           { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
           { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=()' },
           { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' },
-          { key: 'Content-Security-Policy', value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https://tobiaslundh1.pythonanywhere.com" },
+          { key: 'Content-Security-Policy', value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: https:; font-src 'self' data:; connect-src 'self' https://tobiaslundh1.pythonanywhere.com" },
         ]
       }
     ];
